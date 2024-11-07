f545db98ed8ab590ede027aa89ea256785279dcc
Arunjenson-SurveySparrow <arun.jenson@surveysparrow.com>
2024-11-07 19:55:44 +0530

    setted up sanity CMS

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,23 @@
 // import Image from "next/image";
 
+import StartupCard from "@/components/StartupCard";
 import { SearchForm } from "../../components/SearchForm";
 
 export default async function Home({searchParams} : {
   searchParams: Promise<{query?: string}>
 }) {
   const query = (await searchParams).query
+
+  const posts = [{
+    _createdAt: new Date(),
+    views:55,
+    author:{_id:1,name:'Cristian'},
+    _id:1,
+    description:"This is a description",
+    image:'https://unsplash.com/photos/a-close-up-of-a-number-with-flowers-on-it-qN_4d-A2xm8',
+    category:'Robots',
+    title:'We Robots'
+  }]
   return (
     <>
       <section className="pink_container">
@@ -19,6 +31,15 @@ export default async function Home({searchParams} : {
       <p className="text-30-semibold">
         {query ? `Search Results for "${query}" :` : 'All Startups'}
       </p>
+      <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post:StartupCardType,index:number) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+          ):(
+            <p className="no-results">No Startups Found</p>
+          )}
+      </ul>
       </section>
     </>
   );
