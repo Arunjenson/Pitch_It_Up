e42909bdb97e7e07b6e11b391d72779f953657ae
Arunjenson-SurveySparrow <arun.jenson@surveysparrow.com>
2024-11-11 11:04:46 +0530

    added startup and author schemas and added custom grox query by sanity to fetch all the items from sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,22 +2,16 @@
 
 import StartupCard from "@/components/StartupCard";
 import { SearchForm } from "../../components/SearchForm";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({searchParams} : {
   searchParams: Promise<{query?: string}>
 }) {
   const query = (await searchParams).query
 
-  const posts = [{
-    _createdAt: new Date(),
-    views:55,
-    author:{_id:1,name:'Cristian'},
-    _id:1,
-    description:"This is a description",
-    image:'https://unsplash.com/photos/a-close-up-of-a-number-with-flowers-on-it-qN_4d-A2xm8',
-    category:'Robots',
-    title:'We Robots'
-  }]
+  const posts = await client.fetch(STARTUPS_QUERY)
+  console.log(JSON.stringify(posts, null ,2))
   return (
     <>
       <section className="pink_container">
