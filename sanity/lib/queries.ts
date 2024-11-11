c1aa2cf648c38b54cac54484330ef3ce81f214c3
Arunjenson-SurveySparrow <arun.jenson@surveysparrow.com>
2024-11-11 23:50:58 +0530

    added searching startups query

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY =
-  defineQuery(`*[_type == "startup" && defined(slug.current)] |  order(_createdAt desc) {
+  defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
   _id, 
   title, 
   slug,
@@ -99,4 +99,4 @@ export const PLAYLIST_BY_SLUG_QUERY =
     image,
     pitch
   }
-}`);
+}`); 
